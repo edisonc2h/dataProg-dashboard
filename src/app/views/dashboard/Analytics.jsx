09abfaaa14d6d0e197298a2afe1617139952cd0c
Edison Calderon <edison.calderon@code-studios.com>
2023-11-27 01:11:43 -0500

    Delete echart library

--- src/app/views/dashboard/Analytics.jsx
+++ src/app/views/dashboard/Analytics.jsx
@@ -1,8 +1,6 @@
-import { Card, Grid, styled, useTheme } from '@mui/material';
+import { Card, Grid, styled } from '@mui/material';
 import { Fragment } from 'react';
-import DoughnutChart from './shared/Doughnut';
 import StatCards from './shared/StatCards';
-import StatCards2 from './shared/StatCards2';
 
 const ContentBox = styled('div')(({ theme }) => ({
   margin: '30px',
@@ -22,7 +20,6 @@ const SubTitle = styled('span')(({ theme }) => ({
 }));
 
 const Analytics = () => {
-  const { palette } = useTheme();
 
   return (
     <Fragment>
@@ -30,18 +27,12 @@ const Analytics = () => {
         <Grid container spacing={3}>
           <Grid item lg={8} md={8} sm={12} xs={12}>
             <StatCards />
-            <StatCards2 />
           </Grid>
 
           <Grid item lg={4} md={4} sm={12} xs={12}>
             <Card sx={{ px: 3, py: 2, mb: 3 }}>
               <Title>Traffic Sources d</Title>
               <SubTitle>Last 30 days</SubTitle>
-
-              <DoughnutChart
-                height="300px"
-                color={[palette.primary.dark, palette.primary.main, palette.primary.light]}
-              />
             </Card>
           </Grid>
         </Grid>
